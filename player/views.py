1ebc19a25c29e28776c72a8c392731f3951ea2b6
Johannes Rave <johannes.rave@innoq.com>
2021-11-17 02:49:44 +0100

    switched template to showing one play button per song in DB

--- player/views.py
+++ player/views.py
@@ -1,17 +1,16 @@
-from pprint import pprint
-
 from django.shortcuts import render
-
 from player.models import Song
 
 
 def index(request):
     if request.method == 'GET':
         song_id = request.GET.get('song_id')
+
         if song_id:
             song_to_play = Song.objects.get(id=song_id)
         else:
             song_to_play = ''
+
         context = {
             'songs': Song.objects.all(),
             'song_to_play': song_to_play
