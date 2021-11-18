b9a82a1a9e8bf6de5a95ef5bf8da958b94effe54
Johannes Rave <johannes.rave@innoq.com>
2021-11-18 02:03:01 +0100

    added login mechanism using django user management and a login view

--- player/urls.py
+++ player/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from player import views
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', views.Index.as_view, name='player'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('player/', views.Index.as_view(), name='player'),
 ]
--- player/views.py
+++ player/views.py
@@ -3,8 +3,10 @@ from django.views import View
 from player.models import Song
 
 
-# noinspection PyMethodMayBeStatic
-class Index(View):
+class Index(LoginRequiredMixin, View):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
+
     def get(self, request):
         song_id = request.GET.get('song_id')
 
@@ -18,3 +20,30 @@ class Index(View):
             'song_to_play': song_to_play
         }
         return render(request, 'index.html', context)
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return HttpResponse.status_code(403)
+
+        login(request, user)
+        return redirect(request.POST.get('redirect_to', 'player'))
+
+
+class Logout(View):
+    def get(self, request):
+        return redirect('login')
+
+    def post(self, request):
+        intent_to_logout = request.POST.get('logout', False)
+        if intent_to_logout:
+            logout(request)
+            return redirect('login')
