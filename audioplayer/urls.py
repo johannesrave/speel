f7ffc04af13589a7b4f8cf7f28edeb6bd76506bf
FPruss <fpruss@mailbox.org>
2021-11-23 20:05:46 +0100

    working on models

--- audioplayer/urls.py
+++ audioplayer/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('player.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('player.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- player/models.py
+++ player/models.py
@@ -1,7 +1,5 @@
 import uuid
 from django.db import models
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
 
 
 class UUIDModel(models.Model):
@@ -13,7 +11,7 @@ class UUIDModel(models.Model):
 
 
 class Genre(UUIDModel):
-    name = models.CharField(max_length=128, blank=True, default="kein Genre gesetzt")
+    name = models.CharField(max_length=128, blank=False)
 
     def __str__(self):
         return f'{self.name}'
@@ -21,31 +19,32 @@ class Genre(UUIDModel):
 
 class Song(UUIDModel):
     title = models.CharField(max_length=128, blank=True, default="Unbekannter Song")
-    length = models.IntegerField(editable=False, default=1)
+    length = models.IntegerField(editable=False, null=True)
     artists = models.ManyToManyField('Artist', related_name='songs', blank=True, default="Unbekannter Artist")
     genre = models.ForeignKey('Genre', on_delete=models.PROTECT, default='Unbekanntes Genre')
-    album = models.ForeignKey('Album', on_delete=models.PROTECT, default='Unbekanntes Album')
-    file = models.FileField()
+    # album = models.ForeignKey('Album', on_delete=models.PROTECT, default='Unbekanntes Album')
+    audio_file = models.FileField(blank=False)
 
     def __str__(self):
         return f'{self.title}'
 
 
 class Album(UUIDModel):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, blank=False)
     artist = models.ForeignKey('Artist', on_delete=models.PROTECT)
+    songs = models.ManyToManyField('Song', blank=True, related_name='albums')
 
 
 class Playlist(UUIDModel):
-    name = models.CharField(max_length=128)
-    songs = models.ManyToManyField('Song', related_name='playlists')
+    name = models.CharField(max_length=128, blank=False)
+    songs = models.ManyToManyField('Song', blank=True, related_name='playlists')
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Artist(UUIDModel):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, blank=False)
 
     def __str__(self):
         return f'{self.name}'
