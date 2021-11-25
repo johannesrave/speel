5d24544120ed794e47f957f0c7f657be6f193b83
FPruss <fpruss@mailbox.org>
2021-11-25 12:29:11 +0100

    changed admin url to work also without backslash

--- audioplayer/urls.py
+++ audioplayer/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, re_path
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('admin', admin.site.urls),
                   path('', include('player.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
