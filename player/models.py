2fdc79b4dba1c9e5f9598bd457d1763221901840
Johannes Rave <johannes.rave@innoq.com>
2021-12-01 14:47:11 +0100

    do we need all the default entries in manyToMany relationships?

--- player/models.py
+++ player/models.py
@@ -27,8 +27,18 @@ class Artist(UUIDModel):
 class Song(UUIDModel):
     title = models.CharField(max_length=128, blank=True, default="Unbekannter Song")
     length = models.IntegerField(editable=False, null=True)
-    artists = models.ManyToManyField('Artist', blank=True, null=True, related_name='songs', default=Artist.objects.get_or_create(name='Unbekannter Artist')[0])
-    genre = models.ForeignKey('Genre', blank=True, null=True, on_delete=models.PROTECT, default=Genre.objects.get_or_create(name='Unbekanntes Genre')[0].pkid)
+    artists = models.ManyToManyField(
+        'Artist',
+        related_name='songs',
+        blank=True,
+        # default=Artist.objects.get_or_create(name='Unbekannter Artist')[0]
+    )
+    genre = models.ForeignKey(
+        'Genre',
+        blank=True, null=True,
+        on_delete=models.PROTECT,
+        # default=Genre.objects.get_or_create(name='Unbekanntes Genre')[0].pkid
+    )
     audio_file = models.FileField()
 
     def __str__(self):
@@ -37,14 +47,28 @@ class Song(UUIDModel):
 
 class Album(UUIDModel):
     title = models.CharField(max_length=128, blank=False)
-    artists = models.ManyToManyField('Artist', related_name='albums',  default=Artist.objects.get_or_create(name='Unbekannter Artist')[0])
-    songs = models.ManyToManyField('Song', blank=True, null=True, related_name='albums', default=Song.objects.get_or_create(title='Unbekannter Song')[0].pkid)
+    artists = models.ManyToManyField(
+        'Artist',
+        related_name='albums',
+        blank=True,
+        # default=Artist.objects.get_or_create(name='Unbekannter Artist')[0]
+    )
+    songs = models.ManyToManyField(
+        'Song',
+        related_name='albums',
+        blank=True,
+        # default=Song.objects.get_or_create(title='Unbekannter Song')[0].pkid
+    )
 
 
 class Playlist(UUIDModel):
     name = models.CharField(max_length=128, blank=False)
-    songs = models.ManyToManyField('Song', blank=True, null=True, related_name='playlists',
-                                   default=Song.objects.get_or_create(title='Unbekannter Song')[0].pkid)
+    songs = models.ManyToManyField(
+        'Song',
+        related_name='playlists',
+        blank=True,
+        # default=Song.objects.get_or_create(title='Unbekannter Song')[0].pkid
+    )
 
     def __str__(self):
         return f'{self.name}'
