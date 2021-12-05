caf3d31061a0807df6c83b3392ed35e7aee7df9a
Johannes Rave <johannes.rave@innoq.com>
2021-12-05 22:57:30 +0100

    removed unnecessary imports

--- audioplayer/urls.py
+++ audioplayer/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path, re_path
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
