144eea1e02b17fb09513f499fa09f3ae374f13ce
FPruss <fpruss@mailbox.org>
2021-12-07 20:23:54 +0100

    init commit

---
+++ test/test_learning_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+import unittest
+
+
+class TestLearningTests(unittest.TestCase):
+
+    def test_understanding_of_unittest(self):
+        num = 1
+        self.assertEqual(num, 1)
