746d0bda511ddbcc366ac1c1021d25b1909ef35c
Johannes Rave <johannes.rave@innoq.com>
2021-12-10 11:01:38 +0100

    updated and used 'last_song_played'-field of playlist on playing a song via url

--- player/views/player.py
+++ player/views/player.py
@@ -37,11 +37,17 @@ class PlaylistView(GuardedView):
         if len(songs):
             try:
                 song_to_play = songs.get(id=song_id)
-                print(f'Beginning playback at song in playlist: {song_to_play.title}')
+                playlist.last_song_played = song_to_play
+                playlist.save()
+                print(f'Beginning playback at song in playlist and setting as "last_song_played": {song_to_play.title}')
             except Song.DoesNotExist:
                 print(f'Requested nonexistent song with id {str(song_id)}')
-                print(f'Beginning playback at first song in playlist: {songs.first().title}')
-                song_to_play = songs.first()
+                if playlist.last_song_played:
+                    print(f'Beginning playback at last song played in playlist: {songs.first().title}')
+                    song_to_play = playlist.last_song_played
+                else:
+                    print(f'Beginning playback at first song in playlist: {songs.first().title}')
+                    song_to_play = songs.first()
         else:
             print(f'Playlist contains no songs.')
             print(f' Redirecting to playlist selection.')
@@ -59,7 +65,6 @@ class PlaylistView(GuardedView):
 class PlaylistCreateView(GuardedView):
 
     def get(self, request):
-
         context = {
             'action': reverse('create_playlist'),
             'form': PlaylistForm(),
@@ -68,7 +73,6 @@ class PlaylistCreateView(GuardedView):
         return render(request, 'upload/form.html', context)
 
     def post(self, request):
-
         playlist_form = PlaylistForm(request.POST)
 
         if not playlist_form.is_valid():
