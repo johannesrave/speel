adf0adb2a8668ccef25b3f5e5dd3e11950c3d7b9
Johannes Rave <johannes.rave@innoq.com>
2021-12-20 23:35:19 +0100

    added delay to allow postgres to boot and fixed django DB env vars

--- start_django.sh
+++ start_django.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Container booted."
+echo "Waiting for 10 seconds to allow postgres to boot."
+sleep 10
 echo "Making Django migrations."
 python manage.py makemigrations
 echo "Running Django migrations."
