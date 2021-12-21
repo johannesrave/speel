8e753b5ea6dc9eb20ad06f8b81247ece389cb23f
Johannes Rave <johannes.rave@innoq.com>
2021-12-21 11:38:58 +0100

    added longer timeout to allow postgres to boot, added loaddata to seed DB with first users, exposed django on port 80

--- start_django.sh
+++ start_django.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 echo "Container booted."
-echo "Waiting for 10 seconds to allow postgres to boot."
-sleep 10
+echo "Waiting for 30 seconds to allow postgres to boot."
+sleep 30
 echo "Making Django migrations."
 python manage.py makemigrations
 echo "Running Django migrations."
 python manage.py migrate
+echo "Adding default users from fixture."
+python manage.py loaddata seed.json
 echo "Starting Django server."
 python manage.py runserver 8000
