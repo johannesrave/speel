e48cf0101061863aa5bdbc88c5d1be226a4afa74
Johannes Rave <johannes.rave@innoq.com>
2021-12-21 12:18:48 +0100

    added speel-compose.azurewebsites.net to allowed hosts

--- audioplayer/settings.py
+++ audioplayer/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'speel-compose.azurewebsites.net'
+]
 
 # Application definition
 
