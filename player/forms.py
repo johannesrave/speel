70c42e305e47b3ff1b4d57fbeb32e6221e83e57c
Johannes Rave <johannes.rave@innoq.com>
2021-12-21 22:38:52 +0100

    alternative login mechanism using generic form page and form validation

---
+++ player/forms.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, forms, Form, CharField, PasswordInput
+
+from player.models import Song, Playlist, TemporaryFile
+
+
+class LoginForm(Form):
+    username = CharField(label='Benutzername')
+    password = CharField(label='Passwort', widget=PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not (user and user.is_active):
+                raise ValidationError("Benutzername oder Passwort sind nicht korrekt")
+
+
+class SongForm(ModelForm):
+    class Meta:
+        model = Song
+        fields = ['title', 'artists']
+
+
+class PlaylistForm(ModelForm):
+    class Meta:
+        model = Playlist
+        fields = ['name', 'songs', 'thumbnail_file']
+
+
+class TemporaryFileForm(ModelForm):
+    class Meta:
+        model = TemporaryFile
+        fields = ['file']
