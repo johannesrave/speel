763b0cef5a260a0ed096bd6d1c614cc0c9784dfe
Johannes Rave <johannes.rave@innoq.com>
2021-12-21 17:11:33 +0100

    added specific migrations for player app

--- start_django.sh
+++ start_django.sh
@@ -4,6 +4,8 @@ echo "Waiting for 30 seconds to allow postgres to boot."
 sleep 30
 echo "Making Django migrations."
 python manage.py makemigrations
+echo "Making specific player migrations."
+python manage.py makemigrations player
 echo "Running Django migrations."
 python manage.py migrate
 echo "Adding default users from fixture."
