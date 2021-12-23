2829b11330c118e87a387c84009ecbdd9a672447
Johannes Rave <johannes.rave@innoq.com>
2021-12-23 14:36:23 +0100

    trying out django_browser_reload to automatically update the browser when working with templates

--- audioplayer/urls.py
+++ audioplayer/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('player.urls')),
+                  path("__reload__/", include("django_browser_reload.urls")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
