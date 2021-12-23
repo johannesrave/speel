1bb887268f59d25ed2dec59b3eb6b2cd4e5baebf
Johannes Rave <johannes.rave@innoq.com>
2021-12-23 14:36:23 +0100

    trying out django_browser_reload to automatically update the browser when working with templates

--- audioplayer/settings.py
+++ audioplayer/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sass_processor',
     'player',
-    'thumbnails'
+    'thumbnails',
+    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 DJANGO_LIVESYNC = {
