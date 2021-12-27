2bce7e985a78be0b8b9cf89f149e2df8715c90b7
FPruss <fpruss@mailbox.org>
2021-12-27 15:36:43 +0100

    converting wav to mp3 in unittest with audiotools

--- player/test/test_file_conversion.py
+++ player/test/test_file_conversion.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 from unittest import TestCase
-from pydub import AudioSegment
+import audiotools
 
 
 class FileConversionTest(TestCase):
 
     def test_wav_to_mp3(self):
-        mp3_file = AudioSegment.from_wav("/testfiles/music/testFile.wav").export("/testfiles/output/testfile.mp3",
-                                                                                 format="mp3")
+        mp3_file = audiotools.open("testfiles/music/testFile.wav").convert("testfiles/output/testFile.mp3", audiotools.MP3Audio)
+        self.assertIsNotNone(mp3_file)
