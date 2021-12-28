fe1609ca2fa53bcf6fa5fccfeb58810c0879c9c2
FPruss <fpruss@mailbox.org>
2021-12-28 17:47:32 +0100

    manually merging models.py from Johannes working branch, because of errors With missing model 'Track'

--- player/models.py
+++ player/models.py
@@ -1,4 +1,6 @@
 import uuid
+
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from thumbnails.fields import ImageField
 
@@ -18,13 +20,15 @@ class Artist(UUIDModel):
         return f'{self.name}'
 
 
-class Song(UUIDModel):
-    title = models.CharField(max_length=128, blank=True, default="Unbekannter Song")
+class Track(UUIDModel):
+    title = models.CharField(max_length=128, blank=True, default="Unbekannter Track")
     duration = models.IntegerField(editable=False, null=True)
     audio_file = models.FileField()
     artists = models.ManyToManyField(
         'Artist',
-        related_name='songs',
+        through='TrackArtist',
+        through_fields=('track', 'artist'),  # does this even work?
+        related_name='tracks',
         blank=True,
     )
 
@@ -39,8 +43,10 @@ class Playlist(UUIDModel):
     #     on_delete=models.CASCADE,
     #     default=1
     # )
-    songs = models.ManyToManyField(
-        to=Song,
+    tracks = models.ManyToManyField(
+        to='Track',
+        through='PlaylistTrack',
+        through_fields=('playlist', 'track'),  # does this even work?
         related_name='playlists',
         blank=True,
     )
@@ -50,10 +56,14 @@ class Playlist(UUIDModel):
         blank=True,
         null=True
     )
-    last_song_played = models.ForeignKey(
-        to=Song,
+    last_track_played = models.ForeignKey(
+        to='Track',
+        to_field='id',
         blank=True, null=True,
         on_delete=models.SET_NULL
+    )
+    last_timestamp_played = models.IntegerField(
+        blank=True, null=True
     )
 
     def __str__(self):
@@ -62,8 +72,10 @@ class Playlist(UUIDModel):
 
 class Album(UUIDModel):
     title = models.CharField(max_length=128, blank=False)
-    songs = models.ManyToManyField(
-        'Song',
+    tracks = models.ManyToManyField(
+        'Track',
+        through='AlbumTrack',
+        through_fields=('album', 'track'),
         related_name='albums',
         blank=True,
     )
@@ -73,8 +85,29 @@ class Album(UUIDModel):
 
 
 class TemporaryFile(UUIDModel):
-    file = models.FileField()
+    file = models.FileField(upload_to='audio',
+                            validators=[FileExtensionValidator(allowed_extensions=['mp3', 'webm'])])
 
+
+# these explicit relation models are only there to join the M2M-models on their
+# UUIDs instead of their PKIDs
+# this is to enable updating the relationships by sending UUIDs via the HTTP api
+# maybe it would've been easier to just use UUIDs only instead of keeping the bigint PKIDs?
+
+
+class TrackArtist(UUIDModel):
+    track = models.ForeignKey('Track', to_field='id', on_delete=models.CASCADE)
+    artist = models.ForeignKey('Artist', to_field='id', on_delete=models.CASCADE)
+
+
+class PlaylistTrack(UUIDModel):
+    playlist = models.ForeignKey('Playlist', to_field='id', on_delete=models.CASCADE)
+    track = models.ForeignKey('Track', to_field='id', on_delete=models.CASCADE)
+
+
+class AlbumTrack(UUIDModel):
+    album = models.ForeignKey('Album', to_field='id', on_delete=models.CASCADE)
+    track = models.ForeignKey('Track', to_field='id', on_delete=models.CASCADE)
 
 '''
 # got this from https://stackoverflow.com/questions/16041232/django-delete-filefield#16041527
