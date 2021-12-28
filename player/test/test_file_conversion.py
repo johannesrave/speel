1c6cf8b77408e5e5fab288b5aca47e749ac3d86b
FPruss <fpruss@mailbox.org>
2021-12-28 14:10:08 +0100

    started trying tempfile

--- player/test/test_file_conversion.py
+++ player/test/test_file_conversion.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
+import tempfile
 from unittest import TestCase
 import audiotools
 
@@ -9,3 +10,7 @@ class FileConversionTest(TestCase):
     def test_wav_to_mp3(self):
         mp3_file = audiotools.open("testfiles/music/testFile.wav").convert("testfiles/output/testFile.mp3", audiotools.MP3Audio)
         self.assertIsNotNone(mp3_file)
+
+    def test_tempfile(self):
+        tmp_file = tempfile.TemporaryFile()
+        print(tmp_file)
