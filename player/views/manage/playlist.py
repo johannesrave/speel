d3930de4ae5f2a316dbd90027b03961db2f2f2fc
FPruss <fpruss@mailbox.org>
2022-01-03 16:34:42 +0100

    changing playlist.py from method functionality back to class based

--- player/views/manage/playlist.py
+++ player/views/manage/playlist.py
@@ -1,39 +1,60 @@
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from player.forms import PlaylistForm
-from player.models import Playlist
+from player.models import Playlist, Track
+from player.views.views import GuardedView
 
 
-def create_playlist(request):
-    form = PlaylistForm()
+def validate_form(form, request):
+    if form.is_valid():
+        playlist = form.save(commit=False)
+        playlist.owner = request.user
+        playlist.save()
+        return redirect('play_playlist', playlist_id=playlist.id)
 
-    if request.method == 'POST':
+
+class CreatePlaylist(GuardedView):
+
+    @staticmethod
+    def get(request):
+        form = PlaylistForm()
+        context = {
+            'action': reverse('create_playlist'),
+            'form': form,
+            'button_label': 'Playlist erstellen'
+        }
+        return render(request, 'generic/form.html', context)
+
+    @staticmethod
+    def post(request):
         form = PlaylistForm(request.POST, request.FILES)
-        if form.is_valid():
-            playlist = form.save()
-            return redirect('play_playlist', playlist_id=playlist.id)
-
-    context = {
-        'action': reverse('create_playlist'),
-        'form': form,
-        'button_label': 'Playlist erstellen'
-    }
-    return render(request, 'generic/form.html', context)
+        return validate_form(form, request)
 
 
-def update_playlist(request, playlist_id):
-    playlist = Playlist.objects.get(id=playlist_id)
-    form = PlaylistForm(instance=playlist)
+class UpdatePlaylist(GuardedView):
 
-    if request.method == 'POST':
-        form = PlaylistForm(request.POST, instance=playlist)
-        if form.is_valid():
-            playlist = form.save()
-            return redirect('play_playlist', playlist_id=playlist.id)
-    context = {
-        'action': reverse('update_playlist', kwargs={'playlist_id': playlist_id}),
-        'form': form,
-        'button_label': 'Playlist speichern',
-    }
-    return render(request, 'generic/form.html', context)
+    @staticmethod
+    def get(request: HttpRequest, playlist_id):
+        playlist = Playlist.objects.get(id=playlist_id)
+        if playlist in Playlist.objects.filter(owner=request.user):
+            form = PlaylistForm(instance=playlist)
+            action = reverse('update_playlist', kwargs={'playlist_id': playlist_id})
+        else:
+            form = PlaylistForm()
+            action = reverse('update_playlist')
+
+        context = {
+            'action': action,
+            'form': form,
+            'button_label': 'Playlist speichern',
+        }
+        return render(request, 'generic/form.html', context)
+
+    @staticmethod
+    def post(request, playlist_id):
+        playlist = Playlist.objects.get(id=playlist_id)
+        form = PlaylistForm(request.POST, request.FILES, instance=playlist)
+        return validate_form(form, request)
+
