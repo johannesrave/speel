0dc63d22fd26a6595e0e0fc8fa5c2815e887bd81
Johannes Rave <johannes.rave@innoq.com>
2022-01-04 19:21:12 +0100

    put in user registration from Friedemann

---
+++ player/views/register.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+
+
+def register_page(request):
+    form = UserCreationForm
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'action': reverse('register'),
+               'form': form,
+               'button_label': 'Registrieren'}
+    return render(request, 'register.html', context)
