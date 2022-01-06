00d60abb64474c7432c119c74e54ab1d7a835a61
FPruss <fpruss@mailbox.org>
2022-01-06 17:51:09 +0100

    implementing random image picker

--- player/views/manage/playlist.py
+++ player/views/manage/playlist.py
@@ -1,9 +1,45 @@
+import io
+import os
+import random
+
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.utils.datastructures import MultiValueDict
 
+from audioplayer.settings import MEDIA_ROOT
 from player.forms import PlaylistForm
 from player.models import Playlist
 from player.views.views import GuardedView
+
+
+def get_random_default_image():
+    random.seed()
+    value = random.randint(1, 10)
+    return f'{MEDIA_ROOT}/default_images/default_img{value}.jpg'
+
+
+def is_no_image_attached(request):
+    return len(request.FILES) == 0
+
+
+def create_random_image():
+    image_path = get_random_default_image()
+    img = Image.open(image_path)
+    img.save(io.BytesIO(), format='JPEG')
+    return InMemoryUploadedFile(open(image_path, 'rb'),
+                                'image',
+                                image_path,
+                                'image/jpg',
+                                os.path.getsize(image_path),
+                                None,
+                                {})
+
+
+def cleanup_post(post):
+    del post.copy()['image']
+    return post
 
 
 class CreatePlaylist(GuardedView):
@@ -20,7 +56,11 @@ class CreatePlaylist(GuardedView):
 
     @staticmethod
     def post(request):
-        form = PlaylistForm(request.POST, request.FILES)
+        files = request.FILES
+        if is_no_image_attached(request):
+            files = MultiValueDict({'image': [create_random_image()]})
+            request.POST = cleanup_post(request.POST)
+        form = PlaylistForm(data=request.POST, files=files)
         if form.is_valid():
             playlist = form.save()
             return redirect('play_playlist', playlist_id=playlist.id)
@@ -38,7 +78,7 @@ class UpdatePlaylist(GuardedView):
             'action': action,
             'form': form,
             'button_label': 'Playlist speichern',
-            'image': playlist.thumbnail_file
+            'image': playlist.image
         }
         return render(request, 'generic/form.html', context)
 
