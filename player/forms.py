5f694f6d873d05f27d7c5524553325711dd7e702
FPruss <fpruss@mailbox.org>
2022-01-07 10:01:28 +0100

    implemented custom CreateUserForm

--- player/forms.py
+++ player/forms.py
@@ -1,9 +1,17 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm, Form, CharField, PasswordInput, ImageField, FileInput
 from tinytag import TinyTag
 
 from player.models import Track, Playlist, TemporaryFile
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class LoginForm(Form):
