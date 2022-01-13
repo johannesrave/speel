e5012be9e1e2eee3f7ffe52ece70525d5b6bce88
FPruss <fpruss@mailbox.org>
2022-01-13 18:05:31 +0100

    cleanup

--- player/apps.py
+++ player/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
-
 class PlayerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'player'
-
-    def ready(self):
-        import player.signals
