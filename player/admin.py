9e09d917ac309fa77399f7a88115a7a38e0a5bb6
FPruss <fpruss@mailbox.org>
2022-01-13 12:04:50 +0100

    adding Owner to admin

--- player/admin.py
+++ player/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.apps import apps
-from player.models import Playlist, Track
+from player.models import Playlist, Track, Owner
 
 models = apps.get_models()
 
 
-for model in [Playlist, Track]:
+for model in [Playlist, Track, Owner]:
     try:
         admin.site.register(model)
     except admin.sites.AlreadyRegistered:
