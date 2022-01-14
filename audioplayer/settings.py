0ed1cfbf22867e270f29bf13e27aa8ef5cea1710
FPruss <fpruss@mailbox.org>
2022-01-14 19:33:20 +0100

    pulling email secrets from .env

--- audioplayer/settings.py
+++ audioplayer/settings.py
@@ -123,8 +123,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'audiobookplayer.speel@gmail.com'
-EMAIL_HOST_PASSWORD = 'hNnFy_7#TSme9<[2u&Xh'
+EMAIL_HOST_USER = env("EMAIL_ADMIN")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
+PASSWORD_RESET_TIMEOUT_DAYS = 1
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
