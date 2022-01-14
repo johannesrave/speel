91e470fe8162c3a1441047a3c753624344f069dd
FPruss <fpruss@mailbox.org>
2022-01-14 13:45:13 +0100

    added urls for password reset

--- player/urls.py
+++ player/urls.py
@@ -1,14 +1,26 @@
 from django.urls import path
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 from player.models import Playlist, Track
 from player.views import player, login, register, api, library, manage
 from player.views.manage import account, playlist
 
 urlpatterns = [
+    # account pages
     path('login/', login.Login.as_view(), name='login'),
     path('register/', register.Register.as_view(), name='register'),
     path('account/', account.UpdateAccount.as_view(), name='account'),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name='forms/password_reset.html'),
+         name='reset_password'),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name='pages/password_reset_send.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 
     # player pages ('kids' pages)
     path('library/', library.ViewLibrary.as_view(), name='library'),
